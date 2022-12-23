4bacf24f6b8df829a8f96862ce8679763e4bc84a
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2022-12-23 07:24:21 +0100

    add & firebase auth

--- app.py
+++ app.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy # version 2.5
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+import pdb
+import firebase_admin
+from firebase_admin import auth
+import os
 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/jeanlecigne/code/project/BOOK/MTG-TOP8-COMPARATOR/key/mtg-app-eaeb6-firebase-adminsdk-h1f61-abef420e66.json'
+firebase_admin.initialize_app(options={
+    'serviceAccountId': 'lecignejean@gmail.com',
+    'privateKey': '/Users/jeanlecigne/code/project/BOOK/MTG-TOP8-COMPARATOR/key/mtg-app-eaeb6-firebase-adminsdk-h1f61-abef420e66.json'
+})
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
@@ -24,9 +33,12 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(40), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
 class RegisterForm(FlaskForm):
+    email = EmailField(validators=[InputRequired(), Length(
+        min=4, max=40)], render_kw={"placeholder": "Email"})
     username = StringField(validators=[InputRequired(), Length(
         min=4, max=20)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[InputRequired(), Length(
@@ -34,10 +46,13 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Register")
 
     def validate_username(self, username):
+        # pdb.set_trace()
         existing_user_name = User.query.filter_by(
             username=username.data).first()
         if existing_user_name:
             raise ValidationError("That username already exists. Please choose a different one")
+        user = auth.create_user(email=self.email.data, password=self.password.data)
+        print("=== USER UID ===\n", user.uid)
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(
@@ -77,7 +92,7 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
