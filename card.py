160d673edae1666856e554dc59021aa1c6cf0248
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2023-01-04 11:07:31 +0200

    Change card class

--- card.py
+++ card.py
@@ -1,13 +1,28 @@
 import scrython
 import time
+# import asyncio
+
+def get_img(name):
+        time.sleep(0.1)
+        print("===== IN GET IMG ======")
+        print("===== NAME => ", name)
+        card = scrython.cards.Named(fuzzy=name)
+        print("===== CARD ======\n", card)
+        card_image = card.image_uris()
+        card_image_large = card_image['large']
+        return card_image_large
 
 class Card():
     def __init__(self, name):
         self.name = name
+        self.image = get_img(name=name)
     
-    def get_img(name):
-        time.sleep(0.1)
-        card = scrython.cards.Named(fuzzy=name)
-        card_image = card.image_uris()
-        card_image_large = card_image['large']
-        return card_image_large
+    # async def get_img(name):
+    #     time.sleep(0.1)
+    #     print("===== IN GET IMG ======")
+    #     print("===== NAME => ", name)
+    #     card = await scrython.cards.Named(fuzzy=name)
+    #     print("===== CARD ======\n", card)
+    #     card_image = card.image_uris()
+    #     card_image_large = card_image['large']
+    #     return card_image_large
